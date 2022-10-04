6535a5c7f5f88aa5b7b7ba909c478c4c0bdc488f
Morgan Kelly <mkelly5380@gmail.com>
2022-10-04 17:23:34 -0500

    goes through all the times on the scheudler and compares to the current time

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-var date = moment();
 
+//using JQuery functions to add todays date
+var date = moment();
+//formating the date in Day, Month date and then year.
 var currentDate = date.format('dddd, MMM Do YYYY');
 
 console.log(currentDate);
-
+//making the text of the p element with ID current day the currentDate
 $('#currentDay').text(currentDate);
+
+
+//setting the time to the different colors
+
+function determinePastPresentFuture(){
+    //variable to determine the current time
+    var timeNow = moment().hour();
+    //console.log(timeNow);
+
+    $(".time").each(function(){
+        console.log(this);
+        console.log(($(this).attr('id')));
+
+        //making the IDs an integer 
+        var scheduleTime = parseInt($(this).attr('id'));
+        console.log(scheduleTime);
+
+        if(scheduleTime === timeNow){
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else if(scheduleTime < timeNow){
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+
+        }
+        else{
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+
+
+    });
+}
+
+determinePastPresentFuture();
